026b421ca6d24e9135def5211134b32fe8668482
deadpool8525 <krishna.vemuri267@gmail.com>
2017-08-05 14:14:54 +0530

    Initial commit

---
+++ app/src/main/java/krishna/example/com/accelerometer/MainActivity.java
@@ -0,0 +1,23 @@
+package krishna.example.com.accelerometer;
+
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView h,x,y,z;
+    private SensorManager sensorManager;
+    private Sensor sensor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        //initialize views
+        h = (TextView)findViewById(R.id.heading);
+    }
+}
